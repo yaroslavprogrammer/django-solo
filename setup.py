6b293853e958604013f73d708a4498886430ead8
Yaroslav Rudenok <yaroslavprogrammer@gmail.com>
2014-01-13 15:28:53 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+import solo
+
+
+setup(
+    name='django-solo',
+    version=solo.__version__,
+    description=solo.__doc__,
+    packages=find_packages(),
+    url='http://github.com/yaroslavprogrammer/django-solo/',
+    author='lazybird;yaroslavprogrammer',
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    license='Creative Commons Attribution 3.0 Unported',
+)
